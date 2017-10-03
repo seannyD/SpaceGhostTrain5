283f213c68d1dba85da18f06e1ba53ea5aac2194
seannyD <sean.g.roberts@gmail.com>
2017-10-03 21:22:19 +0100

    added script to split video into 3x3

---
+++ offline/scripts/splitVideo.sh
@@ -0,0 +1,17 @@
+
+#crop parameters = width : heigth : start x-axis : start y-axis
+
+rm ../video_crop/*.mp3
+
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:0:0 -acodec copy ../video_crop/1.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw/3:0 -acodec copy ../video_crop/2.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw*2/3:0 -acodec copy ../video_crop/3.mp4
+
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:0:ih/3 -acodec copy ../video_crop/4.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw/3:ih/3 -acodec copy ../video_crop/5.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw*2/3*2:ih/3 -acodec copy ../video_crop/6.mp4
+
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:0:iw*2/3 -acodec copy ../video_crop/7.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw/3:iw*2/3 -acodec copy ../video_crop/8.mp4
+ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw*2/3*2:iw*2/3 -acodec copy ../video_crop/9.mp4
+
