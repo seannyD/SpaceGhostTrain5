8dd6bcf8470075717e854e084f35fa50833afab7
seannyD <sean.g.roberts@gmail.com>
2017-10-03 20:51:10 +0100

    added click functionality to arrows

--- js/room.js
+++ js/room.js
@@ -1,6 +1,29 @@
 
 
 var currentStyle = "black";
+var currentRoomRow = 0;
+var currentRoomCol = 0;
+
+var currentStage = 0;
+
+// note: letters will be upside down!
+var code = [
+  [
+	[1,1,1],
+	[0,1,0],
+	[0,1,0]	
+  ],
+  [
+	[1,1,1],
+	[1,0,1],
+	[1,1,1]	
+  ],
+  [
+	[1,0,0],
+	[1,0,0],
+	[1,1,1]	
+  ]
+];
 
 function switchToBlack(){
 	document.body.style.setProperty('--fg',"white");
@@ -22,7 +45,41 @@ function toggleStyle(){
 	}
 }
 
+function setRoomStyle(){
+	if(currentStyle=="black"){
+		switchToBlack();
+	} else{
+		switchToWhite();
+	}
+}
+
 function start(){
 	console.log("start");
-	setInterval("toggleStyle()", 1000);
+	//setInterval("toggleStyle()", 1000);
+	setRoomColour();
 }
+
+function setRoomColour(){
+	// look up current sytle
+	currentStyle = ["white",'black'][code[currentStage][currentRoomRow][currentRoomCol]];
+	setRoomStyle();
+}
+
+function move(direction){
+	if(direction=="up"){
+		currentRoomRow += 1;
+		setRoomColour();
+	}
+	if(direction=="down"){
+		currentRoomRow -= 1;
+		setRoomColour();
+	}
+	if(direction=="left"){
+		currentRoomCol -= 1;
+		setRoomColour();
+	}
+	if(direction=="right"){
+		currentRoomCol += 1;
+		setRoomColour();
+	}
+}	
