414d3d53d33dcf3dac00e41fe060faf30b479940
seannyD <sean.g.roberts@gmail.com>
2017-10-03 21:35:32 +0100

    added video test page

--- offline/scripts/splitVideo.sh
+++ offline/scripts/splitVideo.sh
@@ -15,3 +15,7 @@ ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:0:iw*2/3 -acodec copy ../video_c
 ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw/3:iw*2/3 -acodec copy ../video_crop/8.mp4
 ffmpeg -i ../video/final.mp4 -vf crop=iw/3:ih/3:iw*2/3*2:iw*2/3 -acodec copy ../video_crop/9.mp4
 
+
+
+# ffmpeg -i ~/Downloads/in.mov -vcodec copy -af "volume=enable='between(t,0,3)':volume=0" ~/Downloads/out2.mov
+
